7ad037361880961bf073cd471bf50f0f1f5f12a7
Petr Kunitsyn <137097270+spasfallen@users.noreply.github.com>
2024-02-05 09:12:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿//Task 1
+
+// int a = 161;
+
+// if (a % 7 == 0 & a % 23 == 0)
+// {
+//     System.Console.WriteLine("the number is multiple a of 7 and 23");
+// }
+// else
+// {
+//     System.Console.WriteLine("the number isn't a multiple of 7 and 23");
+// }
+
+//Task 2
+
+// int x = 2;
+
+// int y = -2;
+
+// if (x > 0 & y > 0)
+// {
+//     System.Console.WriteLine("number of quarter is 1");
+// }
+// else if (x < 0 & y > 0)
+// {
+//     System.Console.WriteLine("number of quarter is 2");
+// }
+// else if (x < 0 & y < 0)
+// {
+//     System.Console.WriteLine("number of quarter is 3");
+// }
+// else
+// {
+//     System.Console.WriteLine("number of quarter is 4");
+// }
+
+//Task 3
+// string str = "96";
+// int max = int.Parse(str[0].ToString());
+
+//             for (int i = 1; i < str.Length; i++)
+//             {
+//                 if (max < int.Parse(str[i].ToString()))
+//                     max = int.Parse(str[i].ToString());
+//             }
+//                 Console.WriteLine("max digit = " + max);
+
+//Task 4
